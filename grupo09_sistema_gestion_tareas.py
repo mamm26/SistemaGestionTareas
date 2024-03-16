5b5589574ec8f0b95f7d43b1bd2c6dc198fafab7
Miguel Madrigal <43477436+mamm26@users.noreply.github.com>
2024-03-16 09:51:27 -0600

    Añadido "Estadísticas Simples" en el menú

--- grupo09_sistema_gestion_tareas.py
+++ grupo09_sistema_gestion_tareas.py
@@ -7,19 +7,28 @@
 # Menu
 # Sólo sale con la opción 6 (Salir)
 menu = "0"
-while menu != "6":
+menuMax = 7
+while menu != str(menuMax):
     print("===== SISTEMA DE GESTIÓN DE TAREAS =====")
     print("1- Agregar Tarea")
     print("2- Ver Tareas pendientes o completadas")
     print("3- Marcar Tarea como completada")
     print("4- Editar o borrar Tareas")
     print("5- Guardar y Cargar Tareas")
-    print("6- Salir del programa")
+    print("6- Estadísticas Simples")
+    print("7- Salir del programa")
 
     menu = input("Digite la opción: ")
 
-    if menu != "1" and menu != "2" and menu != "3" and menu != "4" and menu != "5" and menu != "6":
-        print("¡Error! La opción no es válida.")
+    # Verificamos que exista la opción en el menú
+    menuValido = False
+    for m in range(menuMax):
+        if menu == str(m + 1):
+            menuValido = True
+            break
+
+    if menuValido == False:
+        print(f"¡Error! La opción no es válida. Elige entre 1-{menuMax}.")
         menu = "0"
 
     # Agregar Tarea
@@ -37,8 +46,10 @@ while menu != "6":
     # Guardar y Cargar Tareas
     elif menu == "5":
         print("Próximamente...")
-    # Salir
     elif menu == "6":
+        print("Próximamente...")
+    # Salir
+    elif menu == str(menuMax):
         print("¡HASTA LUEGO!")
 
         
